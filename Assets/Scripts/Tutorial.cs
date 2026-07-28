930a81d8e9f38a468481941da18606e4862f8230
Camber Boles <camberb51@gmail.com>
2026-07-28 11:36:33 -0400

    Placeholder for Neptune tutorial, other planetary level data (but not terrain)

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private RectTransform tutorialRect;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
